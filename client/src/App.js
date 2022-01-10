2d9da168fa103371f1114f017cd04d78a7ad7bc0
sone <sthaya@gmail.com>
2022-01-09 19:08:26 -0800

    setup styled components wrapper

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,8 @@
 import Landing from "./pages/Landing";
-import styled from "styled-components";
-
-const Button = styled.button`
-  background: red;
-  color: white;
-  font-size: 1rem;
-`;
 
 const App = () => {
   return (
     <div>
-      <Button>click</Button>
       <Landing />
     </div>
   );
