29325ff476709d7f0a7af206f6f73bdb6d8ce912
sone <sthaya@gmail.com>
2022-01-22 17:58:55 -0800

    setup middle ware not found route

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import express from "express";
+import notFoundMiddleware from "./middleware/not-found.js";
+
 const app = express();
+
+// middleware
+notFoundMiddleware;
 
 app.get("/", (req, res) => {
   res.send("welcome");
 });
+
+app.use(notFoundMiddleware);
 
 const port = process.env.PORT || 5000;
 
