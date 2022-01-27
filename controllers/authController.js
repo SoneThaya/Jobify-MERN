452e73e0d00ee450fea0aa0e59362d796c639741
sone <sthaya@gmail.com>
2022-01-26 16:41:09 -0800

    setup error codes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,9 @@
 import User from "../models/User.js";
+import { StatusCodes } from "http-status-codes";
 
 const register = async (req, res) => {
-  try {
-    const user = await User.create(req.body);
-    res.status(201).json({ user });
-  } catch (error) {
-    res.status(500).json({ msg: "There was an error!" });
-    console.log(error);
-  }
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
